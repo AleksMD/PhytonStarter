f67d24edc82039789e1d17bd1fde8b981c0e9a3e
AleksMD <47499681+AleksMD@users.noreply.github.com>
2019-02-16 09:07:56 +0200

    Add files via upload

---
+++ PythonTask3.py
@@ -0,0 +1,13 @@
+# The target of this task is to output all natural numbers between 0 and userdefined number.
+user_input = float(input('Number: '))
+x = 0
+
+while x <= int(user_input):
+    print(x)
+    x = x +1
+
+
+
+
+
+
